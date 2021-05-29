45f0a5bbbc8509365f20742a3386da588d4b717e
LiZimo <zimoli@uchicago.edu>
2021-05-29 15:20:04 -0700

    working at this point

--- demo.py
+++ demo.py
@@ -19,6 +19,15 @@ from animate import normalize_kp
 from scipy.spatial import ConvexHull
 import face_alignment
 from glob import glob
+from PIL import Image
+from random import randint
+
+from pynput.keyboard import Key, Listener ## get keyboard input
+
+# import sys
+# sys.path.append("./pixel2style2pixel")
+# from pSpInfer import pSpInfer
+
 
 
 if sys.version_info[0] < 3:
@@ -80,17 +89,17 @@ def make_animation(source_image, driving_video, generator, kp_detector, relative
 			predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
 	return predictions
 
-def single_pass(source, initial_image, target_image, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
+def single_pass(source, target_image, generator, kp_detector, kp_source, kp_driving_initial, relative=True, adapt_movement_scale=True, cpu=False):
 	with torch.no_grad():
 		#predictions = []
 		#source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
 		target_image = torch.tensor(target_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
-		initial_image = torch.tensor(initial_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
+		#initial_image = torch.tensor(initial_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
 		#if not cpu:
 			#source = source.cuda()
 
-		kp_source = kp_detector(source)
-		kp_driving_initial = kp_detector(initial_image)
+		#kp_source = kp_detector(source)
+		#kp_driving_initial = kp_detector(initial_image)
 
 		#print(source.shape)
 		#input('source shape')
@@ -107,9 +116,10 @@ def single_pass(source, initial_image, target_image, generator, kp_detector, rel
 							   kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
 							   use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
 		out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
-		prediction = np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0]
+		#prediction = np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0]
 
-	return prediction
+	#return prediction
+	return out['prediction'].data
 		#predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
 
 
@@ -124,7 +134,8 @@ def auto_crop_resize_face(frame, fa):
 	bboxes = fa.face_detector.detect_from_image(frame[..., ::-1])
 	if len(bboxes) == 0:
 		#return []
-		return frame
+		#return frame
+		return []
 	#return np.array(bboxes)[:, :-1] * scale_factor
 	#print(bboxes)
 	#input('bboxes')
@@ -159,7 +170,7 @@ def auto_crop_resize_face(frame, fa):
 
 	return out_box
 
-def stream_to_webcam(source_image, reader, generator, kp_detector, face_alignment_instance, relative=True, adapt_movement_scale=True, cpu=False, bbox_avg_frames = 10, check_ref_frame_rate = 3):
+def stream_to_webcam(source_image, reader, generator, kp_detector, face_alignment_instance, relative=True, adapt_movement_scale=True, cpu=False, bbox_avg_frames = 7, check_ref_frame_rate = 3):
 	bbox_queue = []
 	with torch.no_grad():
 		#predictions = []
@@ -174,7 +185,8 @@ def stream_to_webcam(source_image, reader, generator, kp_detector, face_alignmen
 
 			#### crop and detect relevant part of face
 			new_bbox = auto_crop_resize_face(frame, face_alignment_instance)
-			bbox_queue.append(new_bbox)
+			if len(new_bbox) != 0:
+				bbox_queue.append(new_bbox)
 			if len(bbox_queue) > bbox_avg_frames:
 				bbox_queue = bbox_queue[-bbox_avg_frames:]
 
@@ -237,9 +249,10 @@ def stream_to_webcam(source_image, reader, generator, kp_detector, face_alignmen
 			# if (m & 0xff == ord('a')):
 			# 	break
 
-def stream_to_webcam_multisource(source_dir, reader, generator, kp_detector, face_alignment_instance, relative=True, adapt_movement_scale=True, cpu=False, bbox_avg_frames = 10, check_ref_frame_rate = 3, cycle_time = 30):
+def stream_to_webcam_multisource(source_dir, reader, generator, kp_detector, face_alignment_instance, relative=True, adapt_movement_scale=True, cpu=False, bbox_avg_frames = 9, check_ref_frame_rate = 5, cycle_time = 30, pspInfer_net = None):
 	bbox_queue = []
-	source_files = glob(source_dir + '/**/*.*', recursive=True)
+	source_dir_basic = source_dir + '/basic'
+	source_dir_priority = source_dir + '/priority'
 	source_files_ind = 0
 	with torch.no_grad():
 
@@ -247,21 +260,31 @@ def stream_to_webcam_multisource(source_dir, reader, generator, kp_detector, fac
 		for i, frame in tqdm(enumerate(reader)):
 
 			if i % cycle_time == 0:
+
+				if randint(0,2) == 2:
+					source_dir = source_dir_basic
+				else:
+					source_dir = source_dir_priority
+
 				source_files = glob(source_dir + '/**/*.*', recursive=True)
-				source_image = imageio.imread(source_files[source_files_ind])
+				source_image = imageio.imread(source_files[source_files_ind % len(source_files)])
 				source_image = resize(source_image, (256, 256))[..., :3]
 				source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
 				source_files_ind += 1
-				source_files_ind = source_files_ind % len(source_files)
+				#source_files_ind = source_files_ind % len(source_files)
 				if not cpu:
 					source = source.cuda()
+
+				kp_source = kp_detector(source)
 
 			reader_frame = frame
 			
 
 			#### crop and detect relevant part of face
 			new_bbox = auto_crop_resize_face(frame, face_alignment_instance)
-			bbox_queue.append(new_bbox)
+			#bbox_queue.append(new_bbox)
+			if len(new_bbox) != 0:
+				bbox_queue.append(new_bbox)
 			if len(bbox_queue) > bbox_avg_frames:
 				bbox_queue = bbox_queue[-bbox_avg_frames:]
 
@@ -284,39 +307,64 @@ def stream_to_webcam_multisource(source_dir, reader, generator, kp_detector, fac
 			frame = resize(frame, (256, 256))[..., :3]
 			if i == 0:
 				reference_frame = frame
+				reference_frame = torch.tensor(reference_frame[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
+				kp_driving_initial = kp_detector(reference_frame)
 
 			### check once per while to see if we can get a better reference_frame
 			if i % check_ref_frame_rate == 0:
 				#print('checking reference_frame')
 
-				indices = [3,4,12,13] + list(range(48,60))
+				indices = list(range(48,60)) #+ [3,4,12,13]
 
-				kp_source_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * source_image)[0] )[indices,:]
-				kp_driving_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * frame)[0] )[indices,:]
+				#try:
+				#	kp_source_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * source_image)[0] )[indices,:]
+				#	kp_driving_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * frame)[0] )[indices,:]
+#
+				#	#kp_source_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * source_image)[0] )#[indices,:]
+				#	#kp_driving_norm = area_normalize_kp ( face_alignment_instance.get_landmarks(255 * frame)[0] )#[indices,:]
+#
+				#	#source_ratio = np.abs(kp_source_norm[48,:] - kp_source_norm[54,:])/np.abs(kp_source_norm[51,:] - kp_source_norm[57,:])
+				#	#driving_ratio = np.abs(kp_driving_norm[48,:] - kp_driving_norm[54,:])/np.abs(kp_driving_norm[51,:] - kp_driving_norm[57,:])
+				#	
+				#	#print(kp_source_norm.shape)
+				#	#print(kp_driving_norm.shape)
+				#	#input('keypoint shape')
+		#
+				#	new_norm = (np.abs(kp_source_norm - kp_driving_norm) ** 2).sum()
+				#	#ew_norm = (np.abs(source_ratio - driving_ratio) ** 2).sum()
+				#	if new_norm < norm:
+				#		norm = new_norm
+				#		reference_frame = frame
+				#		print("current norm is:", norm)
+				#		print('using new reference_frame')
+				#except:
+				#	print("detected no new landmarks for this frame")
 
-				#print(kp_source_norm.shape)
-				#print(kp_driving_norm.shape)
-				#input('keypoint shape')
-	
-				new_norm = (np.abs(kp_source_norm - kp_driving_norm) ** 2).sum()
-				if new_norm < norm:
-					norm = new_norm
-					reference_frame = frame
-					print("current norm is:", norm)
-					print('using new reference_frame')
-
-			output = single_pass(source, reference_frame, frame, generator, kp_detector, relative=relative, adapt_movement_scale=adapt_movement_scale, cpu=cpu)
-			#output_bgr = output[..., ::-1]
-
-			#print(frame.shape)
+			output = single_pass(source, frame, generator, kp_detector, kp_source, kp_driving_initial, relative=relative, adapt_movement_scale=adapt_movement_scale, cpu=cpu)
 			#print(output.shape)
-			#input('shapes')
+			#input('output_shape')
+			#np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0]
+			output = np.transpose(output.cpu().numpy(), [0, 2, 3, 1])[0]
+			#print(output.max())
+			#print(output.min())
+			#input('output shape')
+			if pspInfer_net is not None:
+				output = Image.fromarray( (output * 255).astype(np.uint8))
+				#output = (output - 0.5)/0.5
+				output = pspInfer_net.process(output)
+				output = np.array(output)
+			#else:
+				
+			
+			
+			#print(output.shape)
+			#input('output shape after psp net')
 
-			concatenated = np.concatenate((frame,output), axis = 1)
+			#concatenated = np.concatenate((frame,output), axis = 1)
 
-			cv2.imshow('image', concatenated[..., ::-1])
+			#cv2.imshow('image', concatenated[..., ::-1])
 			#cv2.imshow('image', frame[..., ::-1])
-			#cv2.imshow('image', output[..., ::-1])
+			cv2.imshow('image', output[..., ::-1])
 			k = cv2.waitKey(20)
 			if (k & 0xff == ord('q')):
 				break
@@ -371,7 +419,7 @@ if __name__ == "__main__":
 
 	parser.add_argument("--source_image", default='sup-mat/source.png', help="path to source image")
 	parser.add_argument("--source_dir", default=None, help="path to directory of source images")
-	parser.add_argument("--cycle_time", default=10, help="time in seconds of changing source image, if source_dir is given")
+	parser.add_argument("--cycle_time", default=25, help="time in seconds of changing source image, if source_dir is given")
 	parser.add_argument("--driving_video", default='sup-mat/source.png', help="path to driving video")
 	parser.add_argument("--result_video", default='result.mp4', help="path to output")
  
@@ -386,7 +434,8 @@ if __name__ == "__main__":
  
 	parser.add_argument("--cpu", dest="cpu", action="store_true", help="cpu mode.")
 	parser.add_argument("--webcam", action="store_true", default = False, help = "stream result to webcam")
-
+	parser.add_argument("--psp", action="store_true", default = False, help = "use pixel2style2pixel conversion in output")
+	parser.add_argument("--psp_size", type=int, default=1024, help = "size of psp output")
 
 	parser.set_defaults(relative=False)
 	parser.set_defaults(adapt_scale=False)
@@ -428,6 +477,13 @@ if __name__ == "__main__":
 
 
 	else:
+		pspInfer_net = None
+		if opt.psp:
+			ckpt_path = "./pixel2style2pixel/checkpoints/psp_ffhq_encode.pt"
+			print('loading pspInfer_net...')
+			pspInfer_net = pSpInfer(ckpt_path, opt.psp_size)
+
+
 		device = 'cpu' if opt.cpu else 'cuda'
 		fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device=device)
 		reader = imageio.get_reader("<video0>")
@@ -437,4 +493,4 @@ if __name__ == "__main__":
 			source_image = resize(source_image, (256, 256))[..., :3]
 			stream_to_webcam(source_image, reader, generator, kp_detector, fa, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
 		else: 
-			stream_to_webcam_multisource(opt.source_dir, reader, generator, kp_detector, fa, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu, cycle_time= int (opt.cycle_time * 3))
+			stream_to_webcam_multisource(opt.source_dir, reader, generator, kp_detector, fa, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu, cycle_time= int (opt.cycle_time * 10), pspInfer_net = pspInfer_net)
